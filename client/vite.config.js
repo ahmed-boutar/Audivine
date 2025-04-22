0abec4693230ead653c665100c57b45847ebaf57
Ahmed Boutar <ahmedboutar@Ahmeds-MacBook-Pro-6.local>
2025-04-21 23:45:03 -0400

    Added a placeholder logo

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000', // assuming Flask runs on port 5000
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/Audivine/',
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://127.0.0.1:5000', // assuming Flask runs on port 5000
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
